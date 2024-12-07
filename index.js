6706ff7eba1a6fd12ee7bf8bc08f415eba26df30
mohibbullah272 <bullahmohi50@gmail.com>
2024-12-07 20:06:15 +0600

    add deploy releted update

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+require('dotenv').config()
 const express = require('express')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors')
@@ -6,14 +8,12 @@ app.use(cors())
 app.use(express.json())
 const port = process.env.PORT || 5500
 
-// chill-gammer
-// C6iw9pXzYo92ikBE
 
 
 
 
 
-const uri = "mongodb+srv://chill-gammer:C6iw9pXzYo92ikBE@cluster0.3t5vk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3t5vk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 
 const client = new MongoClient(uri, {
@@ -27,7 +27,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 const reviewCollection =client.db('reviewDB').collection('review')
 const watchListCollection =client.db('watchDB').collection('watchList')
 
@@ -100,6 +100,13 @@ app.get('/sortReviews',async(req,res)=>{
   res.send(result)
 
 })
+app.get('/genresReview',async(req,res)=>{
+  const type = req.query.genres
+  console.log(type)
+  const query ={genres:type}
+  const result=await reviewCollection.find(query).toArray()
+  res.send(result)
+})
 // watchList db start 
 
 app.post('/watchList',async(req,res)=>{
@@ -116,8 +123,8 @@ app.get('/watchList/:email',async(req,res)=>{
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     
